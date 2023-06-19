9f855e90cd7272b66abd7351dd3d1d40612ffa68
thfyth <thfythbhn@163.com>
2023-06-19 11:26:25 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -5,11 +5,11 @@ def main():
     from jmcomic import str_to_list, download_album
     # 下载漫画
     download_album(str_to_list('''
-296575
-145802
-247549
-143826
-212138
+217299
+344474
+149000
+269069
+227153
 '''), option=get_option())
 
 
